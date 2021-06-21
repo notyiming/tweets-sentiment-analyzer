62980c5c08a28138b7332f51108e12ac37d20f60
yiming <ymtan.2062@gmail.com>
2021-06-21 03:13:55 +0100

    added chartjs

--- app.py
+++ app.py
@@ -24,13 +24,27 @@ def get_tweets(api, username, num_of_tweets):
         page += 1
 
 
-def getPolarityTag(polarity_score):
+def get_polarity_tag(polarity_score):
     if polarity_score < 0:
         return "Negative"
     elif polarity_score == 0:
         return "Neutral"
     elif polarity_score > 0:
         return "Positive"
+
+
+def get_tag_num(polarity_tag_list):
+    pos = 0
+    neg = 0
+    neu = 0
+    for tag in polarity_tag_list:
+        if tag == "Positive":
+           pos += 1
+        elif tag == "Negative":
+            neg += 1
+        elif tag == "Neutral":
+            neu += 1
+    return pos, neg, neu
 
 
 @app.route("/", methods=["POST", "GET"])
@@ -42,16 +56,19 @@ def home():
         polarity_score_list = []
         polarity_tag_list = []
         polarity_subjectivity_list = []
+
         for tweet in tweet_text_list:
             edu = TextBlob(tweet)
             polarity_score = edu.sentiment.polarity
             polarity_subjectivity = edu.sentiment.subjectivity
-            polarity_tag = getPolarityTag(polarity_score)
+            polarity_tag = get_polarity_tag(polarity_score)
             polarity_score_list.append(polarity_score)
             polarity_tag_list.append(polarity_tag)
             polarity_subjectivity_list.append(polarity_subjectivity)
+
+        pos, neg, neu = get_tag_num(polarity_tag_list)
         return render_template("index.html", tweets=tweet_text_list, score=polarity_score_list, tag=polarity_tag_list,
-                               subjectivity=polarity_subjectivity_list, username=username)
+                               subjectivity=polarity_subjectivity_list, username=username, pos=pos, neg=neg, neu=neu)
     else:
         return render_template("index.html")
 
